693610d408767a0278212a4c7c8493b093cf7323
Salahuddin FA <salahuddin.fa1@finaccel.co>
2024-04-05 22:53:50 +0700

    fix before submit

--- models/user.js
+++ models/user.js
@@ -1,19 +1,35 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    accountNumber: String,
-    emailAddress: String,
-    identityNumber: String
+    username: {
+        type: String,
+        index: true,
+        unique: true
+    },
+    accountNumber: {
+        type: String,
+        index: true,
+        unique: true
+    },
+    emailAddress: {
+        type: String,
+        index: true,
+        unique: true
+    },
+    identityNumber: {
+        type: String,
+        index: true,
+        unique: true
+    }
 });
 
 // Create a virtual property 'id' which maps to '_id' field
-userSchema.virtual('id').get(function() {
+userSchema.virtual('id').get(function () {
     return this._id.toHexString();
 });
 
 // Ensure virtual fields are serialized when converting to JSON
-userSchema.set('toJSON', { virtuals: true });
+userSchema.set('toJSON', {virtuals: true});
 
 const User = mongoose.model('User', userSchema);
 
